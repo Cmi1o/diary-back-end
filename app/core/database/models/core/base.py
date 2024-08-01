348794448812bd3d017722e8673b4bfa4b3da3aa
Georgiy Vasin <georgijvasin01@gmail.com>
2024-08-01 16:44:52 +0300

    add new fields to declarative base

--- app/core/database/models/core/base.py
+++ app/core/database/models/core/base.py
@@ -5,9 +5,14 @@ from sqlalchemy.orm import (
 )
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
+from .annotations import time
+
 
 class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[time]
+    updated_at: Mapped[time]
+    
     
     @declared_attr.directive
     def __tablename__(cls) -> str:
