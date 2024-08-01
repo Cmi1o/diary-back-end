4ff1b5df1596efda93d9996e0345f69fd750576f
Georgiy Vasin <georgijvasin01@gmail.com>
2024-08-01 15:55:37 +0300

    add base annotations for tables columns

---
+++ app/core/database/models/core/annotations.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from datetime import datetime
+
+from sqlalchemy import (
+    String, 
+    DateTime,
+)
+from sqlalchemy.orm import mapped_column
+
+
+__all__ = (
+    'str255', 
+    'str50', 
+    'integer', 
+    'time'
+)
+
+
+str50 = Annotated[str, mapped_column(String(50))]
+str255 = Annotated[str, mapped_column(String(255))]
+integer = Annotated[int, mapped_column()]
+time = Annotated[datetime, mapped_column(DateTime())]
