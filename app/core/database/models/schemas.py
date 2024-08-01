04e296029cd5a7cdbccbac8a0819f99443ae7306
Georgiy Vasin <georgijvasin01@gmail.com>
2024-08-02 02:08:28 +0300

    add relations between tables

--- app/core/database/models/schemas.py
+++ app/core/database/models/schemas.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column, relationship
 
 from .core import Model
 from .core.annotations import *
@@ -8,8 +12,13 @@ class User(Model):
     name: Mapped[str50]
     password: Mapped[integer]
     salt_hash: Mapped[integer]
+    
+    tasks: Mapped[List['Task']] = relationship(back_populates='user')
 
 
 class Task(Model):
     name: Mapped[str50]
     description: Mapped[str255]
+    
+    user_id: Mapped[integer] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['User'] = relationship(back_populates='tasks')
