f6e3e975e354c737e687c16d033978d4192c8496
Georgiy Vasin <georgijvasin01@gmail.com>
2024-08-07 12:36:09 +0300

    fix create method in Table that always creates a User

--- app/core/database/tables_managers/table.py
+++ app/core/database/tables_managers/table.py
@@ -3,50 +3,49 @@ from sqlalchemy import sql
 
 from app.core.database.models import Model
 from app.core.database.models.core import session_factory
-from app.core.database.models.schemas import User
 
 from .base import ITableManager
 
 
 class Table[_MT: Model](ITableManager[_MT]):
     def __init__(self, table: _MT) -> None:
-        self.table = type(table)
+        self.__table = type(table)
     
     async def get_by(self, **filter_data):
         async with session_factory() as session:
             return await session.scalar(
-                sql.select(self.table).filter_by(
+                sql.select(self.__table).filter_by(
                     **filter_data
                 )
             )
     
     async def get_all(self) -> AsyncGenerator[_MT, None]:
         async with session_factory() as session:
-            stmt = sql.select(self.table)
+            stmt = sql.select(self.__table)
             async for row in await session.stream(stmt):
                 yield row[0]
     
     async def get_many_with(self, **filter_data) -> AsyncGenerator[_MT, None]:
         async with session_factory() as session:
-            stmt = sql.select(self.table).filter_by(**filter_data)
+            stmt = sql.select(self.__table).filter_by(**filter_data)
             async for row in await session.stream(stmt):
                 yield row[0]
     
     async def create(self, **data):
         async with session_factory() as session:
-            session.add(User(**data))
+            session.add(self.__table(**data))
             await session.commit()
     
     async def update_by(self, values, **filter_data):
         async with session_factory() as session:
-            stmt = sql.update(self.table).filter_by(**filter_data)
+            stmt = sql.update(self.__table).filter_by(**filter_data)
             
             await session.execute(stmt.values(**values))
             await session.commit()
     
     async def delete_by(self, **filter_data):
         async with session_factory() as session:
-            stmt = sql.delete(self.table).filter_by(**filter_data)
+            stmt = sql.delete(self.__table).filter_by(**filter_data)
             
             await session.execute(stmt)
             await session.commit()
