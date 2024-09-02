286fb7430cb696ce34d9a13befb7a2283a3baa8a
Georgiy Vasin <georgijvasin01@gmail.com>
2024-09-02 23:38:28 +0300

    fixed correct user data update in db and fixed code style

--- app/api/v1/auth/psw_handler.py
+++ app/api/v1/auth/psw_handler.py
@@ -1,6 +1,8 @@
-from app.core.database import controller
 import secrets
+
 from hashlib import sha256
+
+from app.core.database import controller
 
 
 def hash(message: str) -> str:
@@ -8,28 +10,29 @@ def hash(message: str) -> str:
 
 
 # saves psw and salt to db
-async def update_psw(user, new_psw: str):
+async def update_psw(user_name: str, new_psw: str) -> None:
     # fetch user by name
-    db_user = await controller.users.get_by(name=user) 
-    if not db_user:
-        raise ValueError(f"User {user} not found")
+    user = await controller.users.get_by(name=user_name) 
+    if not user:
+        raise ValueError(f'User {user_name} not found')
     
     # create salt and psw hash
     salt = secrets.token_urlsafe(len(new_psw))  # string of random len(psw) bytes in Base64
-    user_psw_hash = hash(salt + new_psw), salt
+    user_psw_hash = hash(salt + new_psw)
     
-    # save to db
-    db_user.password_hash = user_psw_hash
-    db_user.salt = salt
-    
+    await controller.users.update_by(name=user_name, values={
+        'password_hash': user_psw_hash,
+        'salt': salt
+    })
+
 
 # fetches user from db, checks input psw with saved psw
-async def validate_psw(user, psw: str) -> bool:
-    db_user = await controller.users.get_by(name=user)
-    if not db_user:
-        raise ValueError(f"User {user} not found")
+async def validate_psw(user_name: str, psw: str) -> bool:
+    user = await controller.users.get_by(name=user_name)
+    if not user:
+        raise ValueError(f'User {user_name} not found')
     
-    user_psw_hash = db_user.password_hash
-    user_salt = db_user.salt
-    
-    return secrets.compare_digest(user_psw_hash, hash(user_salt + psw))  # timing attack safe
+    # timing attack safe
+    return secrets.compare_digest(
+        user.password_hash, hash(user.salt + psw)
+    )  
